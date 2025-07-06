f3bbba0d64d7a2f60b6aab3e890547360b308983
ethan <fawastudios@gmail.com>
2025-07-05 21:39:10 -0400

    Installed Conan. Start of GLFW3 Integration.
    Contains errors because the CMakeLists.txt refuses to find the GLFW3 header file (mainly because Conan is refusing to make it). I'll have to figure it out later.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
