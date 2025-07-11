9f79e0d56f2f5499365451bbd5bb4dbb4aa2666c
ethan <fawastudios@gmail.com>
2025-07-11 18:57:32 -0400

    Beginning of Triangle Rendering
    - Added vertex_buffer and pipeline to App struct
    - Created vertex.glsl and fragment.glsl shaders
    - Created "src/engine/vertex.rs" with a MyVertex struct

---
+++ src/engine/vertex.rs
@@ -0,0 +1,12 @@
+// engine/renderer/first_triangle.rs
+use vulkano::buffer::BufferContents;
+use vulkano::pipeline::graphics::vertex_input::Vertex;
+
+#[derive(BufferContents, Vertex)]
+#[repr(C)]
+pub struct MyVertex {
+    #[format(R32G32_SFLOAT)]
+    pub position: [f32; 2],
+    #[format(R32G32B32_SFLOAT)]
+    pub color: [f32; 3],
+}
