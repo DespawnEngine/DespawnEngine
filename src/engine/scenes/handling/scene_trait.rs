27a196a0161a461a6f53d604ef2e903bfa26e5cf
ethan <fawastudios@gmail.com>
2025-10-14 17:48:37 -0400

    Added start of proper scene functions.

--- src/engine/scenes/handling/scene_trait.rs
+++ src/engine/scenes/handling/scene_trait.rs
@@ -1,7 +1,28 @@
 use crate::engine::scenes::handling::scene_manager::SceneManager;
 
-pub trait Scene {
-    fn start(&mut self, manager: &SceneManager) {}
-    fn update(&mut self, manager: &SceneManager);
-    fn draw(&self);
+pub trait Scene: Send {
+    fn awake(&mut self, _manager: &SceneManager) {
+        // Initialize systems, load assets. Runs ONCE when scene is created, before Start
+    }
+
+    fn start(&mut self, _manager: &SceneManager) {
+        // Called when the scene becomes active.
+    }
+
+    fn update(&mut self, _manager: &SceneManager) {
+        // Per frame. Main logic. (input, world, AI, etc.)
+    }
+
+    fn fixed_update(&mut self, _manager: &SceneManager) {
+        // Runs on a fixed timestep. Preferred for physics and collisions
+    }
+
+    fn late_update(&mut self, _manager: &SceneManager) {
+        // Runs after update. Good for things like camera follow logic, etc.
+    }
+
+    fn draw(&self)
+    {
+        // Runs after Update, Fixed Update, and Late Update.
+    }
 }
